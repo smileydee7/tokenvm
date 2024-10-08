fe11446f2301dddf61291e7fa2efb098bd7d35ec
smileydee7 <adachidivine7@gmail.com>
2024-10-08 22:12:14 +0100

    last commit

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "SMILEYDEE"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "SMILEY"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "SS"
 )
 
 var ID ids.ID
